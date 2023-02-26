71c8bdb354c3de00b9943e0a34ff13e1d16544c7
miguelorestecoding <miguelorestecoding@gmail.com>
2023-02-25 23:22:48 -0300

    opciones de dolar completo. Iconos agregados.

--- script.js
+++ script.js
@@ -14,28 +14,72 @@
 //  calculaDolarSolidario(solicitaDolarOficialBNVendedor())
 //  mostrarResultado()
 
-
-
 function solicitaDolarOficialBNVendedor() {
-    let dolarOficialBNVendedor = prompt(" Ingresa el Valor del Dolar Oficial, Banco Nación, Vendedor.\n Puedes consultarlo en:\n https://www.bna.com.ar/Personas ")
-    return parseFloat(dolarOficialBNVendedor)
+  let dolarOficialBNVendedor = prompt(
+    " Ingresa el Valor del Dolar Oficial, Banco Nación, Vendedor.\n Puedes consultarlo en:\n https://www.bna.com.ar/Personas "
+  );
+  return parseFloat(dolarOficialBNVendedor);
 }
 
 function calculaDolarSolidario(dolarOficialBNVendedor) {
-    let resultadoDolarSolidario = dolarOficialBNVendedor + (dolarOficialBNVendedor * 30/100) + (dolarOficialBNVendedor * 35/100)
-    return resultadoDolarSolidario
+  let resultadoDolarSolidario =
+    dolarOficialBNVendedor +
+    (dolarOficialBNVendedor * 30) / 100 +
+    (dolarOficialBNVendedor * 35) / 100;
+  return resultadoDolarSolidario;
 }
 
 function calculaDolarNetflix(dolarOficialBNVendedor) {
-    let resultadoDolarNetflix = dolarOficialBNVendedor + (dolarOficialBNVendedor * 30/100) + (dolarOficialBNVendedor * 45/100)
-    return resultadoDolarNetflix
+  let resultadoDolarNetflix =
+    dolarOficialBNVendedor +
+    (dolarOficialBNVendedor * 30) / 100 +
+    (dolarOficialBNVendedor * 45) / 100;
+  return resultadoDolarNetflix;
 }
 
-function mostrarResultado(dolarOficialBNVendedor, resultadoDolarSolidario, resultadoDolarNetflix) {
-    alert(" Los diferentes valores del dolar, con una cotización de dolar oficial a $" + dolarOficialBNVendedor + ", son:\n * $" + resultadoDolarSolidario + " Dolar Solidario \n * $" + resultadoDolarNetflix + " Dolar Netflix")
+function calculaDolarQuatar(dolarOficialBNVendedor) {
+  let resultadoDolarQuatar =
+    dolarOficialBNVendedor +
+    (dolarOficialBNVendedor * 30) / 100 +
+    (dolarOficialBNVendedor * 45) / 100 +
+    (dolarOficialBNVendedor * 25) / 100;
+  return resultadoDolarQuatar;
 }
 
-let dolarOficialBNVendedor = solicitaDolarOficialBNVendedor()
-let resultadoDolarSolidario = calculaDolarSolidario(dolarOficialBNVendedor)
-let resultadoDolarNetflix = calculaDolarNetflix(dolarOficialBNVendedor)
-mostrarResultado(dolarOficialBNVendedor, resultadoDolarSolidario, resultadoDolarNetflix)
+function mostrarResultado(
+  dolarOficialBNVendedor,
+  resultadoDolarSolidario,
+  resultadoDolarNetflix,
+  resultadoDolarQuatar
+) {
+  alert(
+    " Los diferentes valores del  son:\n\n ✅-👮‍♂️ $" +
+      dolarOficialBNVendedor.toLocaleString("es-ES", {
+        minimumFractionDigits: 2,
+      }) +
+      " Dolar Oficial Banco Nación Venedor.\n ✅-🥰 $" +
+      resultadoDolarSolidario.toLocaleString("es-ES", {
+        minimumFractionDigits: 2,
+      }) +
+      " Dolar Solidario. \n ✅-💳 $" +
+      resultadoDolarNetflix.toLocaleString("es-ES", {
+        minimumFractionDigits: 2,
+      }) +
+      " Dolar Tarjeta ó Netflix.\n ✅-✈ $" +
+      resultadoDolarQuatar.toLocaleString("es-ES", {
+        minimumFractionDigits: 2,
+      }) +
+      " Dolar Quatar."
+  );
+}
+
+let dolarOficialBNVendedor = solicitaDolarOficialBNVendedor();
+let resultadoDolarSolidario = calculaDolarSolidario(dolarOficialBNVendedor);
+let resultadoDolarNetflix = calculaDolarNetflix(dolarOficialBNVendedor);
+let resultadoDolarQuatar = calculaDolarQuatar(dolarOficialBNVendedor);
+mostrarResultado(
+  dolarOficialBNVendedor,
+  resultadoDolarSolidario,
+  resultadoDolarNetflix,
+  resultadoDolarQuatar
+);
